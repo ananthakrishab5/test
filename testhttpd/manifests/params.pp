69136dae0b16143f962d4a306da84236ac12f8da
ananthakrishab5 <ananthakrishna.paturi@gmail.com>
2015-09-21 04:14:42 -0400

    testhttpd with params file and a variable in site.pp

---
+++ testhttpd/manifests/params.pp
@@ -0,0 +1,12 @@
+class testhttpd::params
+{
+$package = ' ' 
+$package_ensure = ' '
+$config_file = ' '
+$file_ensure = ' '
+$file_source = ' '
+$service = ' '
+$service_ensure = ' '
+# $service_enable = ' '
+
+}
